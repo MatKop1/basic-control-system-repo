2029e273c4f91aa8874eb2f0c22062e10211b77e
MatKop1 <134069835+MatKop1@users.noreply.github.com>
2023-08-20 18:07:05 +0200

    Testing shared library

--- interface.py
+++ interface.py
@@ -1,10 +1,27 @@
-#import ctypes
+import ctypes
 import tkinter as tk
 from tkinter import ttk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-#mylib = ctypes.CDLL('C:\\Users\\mat3u\\OneDrive\\Dokumenty\\GitHub\\basic-control-system-repo\\build\\Debug\\ControlSys.dll')
+mylib = ctypes.CDLL('C:\\Users\\mat3u\\OneDrive\\Dokumenty\\GitHub\\basic-control-system-repo\\sharedLib.so')
+
+# Define argument and return types
+mylib.output.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_double]
+mylib.output.restype = ctypes.POINTER(ctypes.c_double)
+
+def call_output_function(dStop, dTs, dU):
+    result_ptr = mylib.output(dStop, dTs, dU)
+    result_list = [result_ptr[i] for i in range(100)]  # Assuming the loop runs 100 times
+    mylib.free(result_ptr)  # Free memory allocated by the C++ function
+    return result_list
+
+dStop = 1.0
+dTs = 0.1
+dU = 0.5
+#output_list = call_output_function(dStop, dTs, dU)
+#print(output_list)
+
 # FUNCTIONS
 def plot_data():
     x = [1, 2, 3, 4, 5]
