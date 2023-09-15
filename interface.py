4c8985be4f21316df4d502018544d6f0b583fe8c
matkop <134069835+MatKop1@users.noreply.github.com>
2023-09-15 18:13:26 +0200

    Create GUI

--- interface.py
+++ interface.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 # Defining model 
 def LinearObj(num, den):
     H = control.tf(num, den)
+    print(H)
     return H    
 
 # Defining plot function
@@ -14,12 +15,53 @@ def Plot(H):
     plt.plot(t,y)
     plt.title("Step Response")
     plt.grid()
-    plt.waitforbuttonpress()
+    plt.show()
+
+# Defining Str to List
+def stringToList(str):
+    integer_list = []  # Create an empty list to store the integers
+    
+    # Split the input string by commas and iterate over the parts
+    parts = str.split(',')
+    for part in parts:
+        try:
+            integer = int(part)  # Convert each part to an integer
+            integer_list.append(integer)  # Append the integer to the list
+        except ValueError:
+            pass  # Ignore non-integer parts
+    
+    return integer_list  
+
+# Defining graph
+def graph(entryNum,entryDen):
+    num = stringToList(entryNum.get())
+    den = stringToList(entryDen.get())
+    H = LinearObj(num,den)
+    Plot(H)
 
 # Defining main function
 def main():
-    H = LinearObj([1],[2,1])
-    Plot(H)
+    window = tk.Tk()
+    window.title("Control system interface")
+    window.geometry("400x200")
+
+    labelNum = tk.Label(text="Numerator")
+    labelNum.pack()
+
+    entryNum = tk.Entry()
+    entryNum.pack()
+ 
+    labelDen = tk.Label(text="Denumerator")
+    labelDen.pack()
+
+    entryDen = tk.Entry()
+    entryDen.pack() 
+
+    buttonGen = tk.Button(window, text="Step", command=lambda:graph(entryNum,entryDen))
+    buttonGen.pack()
+    window.mainloop()
+
+
 
 # Main loop
 if __name__=="__main__":
