3d400057804cc69d66c5014a77ab13047ca28ca9
matkop <134069835+MatKop1@users.noreply.github.com>
2023-09-17 20:04:35 +0200

    Create test for stringTolist function

--- TEST/test_control.py
+++ TEST/test_control.py
@@ -14,3 +14,18 @@ class TestControl(unittest.TestCase):
         H1 = control.tf([2],[1,1])
         H2 = inter.LinearObj([1],[2,1])
         self.assertNotEqual(H1.num, H2.num,"Numerator of H1 is not equal to H2")
+
+    def test_strTolsit(self):
+        s1 = "11"
+        s2 = "0,0,0"
+        s3 = "1,12,123,12345"
+        s4 = ",1,"
+        s5 = ",10,"
+        s6 = "1,,2"
+        self.assertEqual(inter.stringToList(s1),[11])
+        self.assertEqual(inter.stringToList(s2),[0,0,0])
+        self.assertEqual(inter.stringToList(s3),[1,12,123,12345])
+        self.assertEqual(inter.stringToList(s4),[1])
+        self.assertEqual(inter.stringToList(s5),[10])
+        self.assertEqual(inter.stringToList(s6),[1,2])
+
